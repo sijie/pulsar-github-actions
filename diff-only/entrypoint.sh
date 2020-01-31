dbe5ada2d77749038046def7ac31e0363d21ec93
Sijie Guo <sijie@apache.org>
2020-01-31 11:41:30 -0800

    Add a `diff_only` action

---
+++ diff-only/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -l
+
+read -r -a TARGET_DIRS <<< "$*"
+
+CHANGED_DIRS=$(git diff --dirstat=files,0 HEAD~$(jq '.commits | length' "${GITHUB_EVENT_PATH}") | awk '{ print $2 }')
+
+found_changed_dir_not_in_target_dirs="no"
+for changed_dir in ${CHANGED_DIRS}
+do
+    matched="no"
+    for target_dir in "${TARGET_DIRS[@]}"
+    do
+        if [[ ${changed_dir} == "${target_dir}"* ]]; then
+            matched="yes"
+            break
+        fi
+    done
+    if [[ ${matched} == "no" ]]; then
+        found_changed_dir_not_in_target_dirs="yes"
+        break
+    fi
+done
+
+if [[ ${found_changed_dir_not_in_target_dirs} == "yes" ]]; then
+    echo "Changes not only in $*, proceeding"
+    echo ::set-output name=diff_only::no
+else
+    echo "Changes only in $*, skipping"
+    echo ::set-output name=diff_only::yes
+fi
